8e0a17229b08220968201b6e8a2b5058acdff3e9
Quan Phan <quanp23709671@gmail.com>
2026-04-18 19:21:51 +0700

    Regiter (UI and Validate)

---
+++ Laptify-Frontend/src/services/auth/authService.js
@@ -0,0 +1,29 @@
+import { axiosClient } from "@/lib/axiosClient.js";
+
+export const authService = {
+  register: async (userData) => {
+    try {
+      const response = await axiosClient.post("/auth/register", userData);
+      return response.data;
+    } catch (error) {
+      throw new Error(error.response?.data?.message || "Đăng ký thất bại");
+    }
+  },
+
+  login: async (credentials) => {
+    try {
+      const response = await axiosClient.post("/auth/login", credentials);
+      return response.data;
+    } catch (error) {
+      throw new Error(error.response?.data?.message || "Đăng ký thất bại");
+    }
+  },
+
+  logout: async () => {
+    try {
+      await axiosClient.post("/auth/logout");
+    } catch (error) {
+      throw new Error(error.response?.data?.message || "Đăng ký thất bại");
+    }
+  },
+};
