a02f96db714eaa8cf1061059cd834f39ce9b646a
Duc Nguyen <nguyenvananhduc2005@gmail.com>
2026-05-17 17:48:05 +0700

    stuff

--- Laptify-Frontend/src/pages/client/product/detail/ProductDetailPage.jsx
+++ Laptify-Frontend/src/pages/client/product/detail/ProductDetailPage.jsx
@@ -166,7 +166,7 @@ const ProductDetailPage = () => {
   }
 
   return (
-    <div className='container mx-auto px-4 py-8'>
+    <div className='max-w-7xl mx-auto px-4 py-8'>
       {/* Breadcrumb */}
       <div className='text-sm text-gray-600 mb-6'>
         Tới khoảng / Bàn phím / {productDetail.name}
